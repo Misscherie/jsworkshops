fab1ac3be57dc8da7830df4beee07e7e8f1bc59a
Ella.koles <ella.koles@yandex.ru>
2022-12-28 15:03:59 -0500

    Added first homework

---
+++ script.js
@@ -0,0 +1,9 @@
+let celcius = prompt("Введите температуру по Цельсиусу");
+let fahrenheit = (9 / 5) * celcius + 32;
+alert(`Температура в фаренгейте равняется ${fahrenheit}`);
+console.log(celcius);
+console.log(fahrenheit);
+
+// let firstname = "Ella";
+// let admin = firstname;
+// console.log(admin);
