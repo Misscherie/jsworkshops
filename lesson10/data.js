49c07bc79de240f09a8a4e590beb4db70a0494d3
Ella.koles <ella.koles@yandex.ru>
2023-02-17 13:03:09 -0500

    Added 10th homework

---
+++ lesson10/data.js
@@ -0,0 +1,34 @@
+// Задание 1
+// 1. Поиск в интернете (бесплатные api примеры).
+// 2. Найти любые данные, на произвольную тему.
+// 3. Создать файл data.js.
+// 4. Создать переменную которая будет хранить данные из публичных api.
+
+// Задание 2
+// 1. Создать файл index.html.
+// 2. Подключить data.js.
+// 3. Сформировать контент из данных (картинка загловок и параграф).
+// 4. Добавить данный контент в вёрстку.
+// 5. * Добавить стили при необходимости (по желанию).
+const data = [
+  {
+    albumId: 1,
+    id: 1,
+    title: "accusamus beatae ad facilis cum similique qui sunt",
+    url: "https://via.placeholder.com/600/92c952",
+    thumbnailUrl: "https://via.placeholder.com/150/92c952",
+  },
+  {
+    albumId: 1,
+    id: 2,
+    title: "reprehenderit est deserunt velit ipsam",
+    url: "https://via.placeholder.com/600/771796",
+    thumbnailUrl: "https://via.placeholder.com/150/771796",
+  },
+];
+data.forEach((obj) => {
+  document.body.insertAdjacentHTML(
+    "afterbegin",
+    `<li> <span> id: ${obj.id} </span> <h2> ${obj.title} </h2> <img src=${obj.url}> </li>`
+  );
+});
